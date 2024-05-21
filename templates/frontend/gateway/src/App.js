9f661884efaf9e4a29c71085b9549988a4e9633b
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-05-21 15:38:36 +0100

    Fix api config in production builds

--- templates/frontend/gateway/src/App.js
+++ templates/frontend/gateway/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import 'error-polyfill'
 import { useInitNear } from 'near-social-vm'
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import 'react-bootstrap-typeahead/css/Typeahead.bs5.css'
 import 'react-bootstrap-typeahead/css/Typeahead.css'
 import {
@@ -18,23 +18,41 @@ export const refreshAllowanceObj = {}
 
 function App(props) {
   const { initNear } = useInitNear()
-  const api = {}
-
-  if (process.env.MODE === 'production') {
-    api.ship = window.ship
-  }
-
-  if (process.env.MODE === 'development') {
-    api.ship = process.env.REACT_APP_SHIP
-    api.url = process.env.REACT_APP_HOST
-    api.code = process.env.REACT_APP_CODE
-  }
+  const [api, setApi] = useState({ ship: '', url: '', code: '' })
 
   useEffect(() => {
-    initNear &&
+    const apiConfig = {}
+
+    if (process && process.env.MODE === 'development') {
+      apiConfig.ship = process.env.SHIP
+      apiConfig.url = process.env.URL
+      apiConfig.code = process.env.CODE
+      setApi(apiConfig)
+    } else {
+      apiConfig.url = window.location.origin
+
+      async function getShip() {
+        const response = await fetch(`${apiConfig.url}/~/name`, {
+          method: 'get',
+          credentials: 'include'
+        })
+        const getStream = await response.text()
+        return getStream.substring(1)
+      }
+
+      getShip().then(ship => {
+        apiConfig.ship = ship
+        setApi(apiConfig)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (initNear) {
       initNear({
         networkId: 'testnet'
       })
+    }
   }, [initNear])
 
   const passProps = {
@@ -45,14 +63,14 @@ function App(props) {
   let str = window.location.pathname
   let before = str.substring(0, str.indexOf(`/gateway`))
   return (
-      <Router basename={`${before}/gateway`}>
-        <Switch>
-          <Redirect exact from="/" to="/example" />
-          <Route path="/example">
-            <ExamplePage {...passProps} />
-          </Route>
-        </Switch>
-      </Router>
+    <Router basename={`${before}/gateway`}>
+      <Switch>
+        <Redirect exact from="/" to="/example" />
+        <Route path="/example">
+          <ExamplePage {...passProps} />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
